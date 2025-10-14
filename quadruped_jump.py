1f86a3edf724da36df1d5d3e14043e6e53da05d0
martinmayer6412 <martin.mayer@net-c.com>
2025-10-14 16:49:50 +0200

    GRAVITY

--- quadruped_jump.py
+++ quadruped_jump.py
@@ -11,7 +11,7 @@ def quadruped_jump():
     # Initialize simulation
     # Feel free to change these options! (except for control_mode and timestep)
     sim_options = SimulationOptions(
-        on_rack=True,  # Whether to suspend the robot in the air (helpful for debugging)
+        on_rack=False,  # Whether to suspend the robot in the air (helpful for debugging)
         render=True,  # Whether to use the GUI visualizer (slower than running in the background)
         record_video=False,  # Whether to record a video to file (needs render=True)
         tracking_camera=True,  # Whether the camera follows the robot (instead of free)
@@ -66,12 +66,11 @@ def quadruped_jump():
     # OPTIONAL: add additional functions here (e.g., plotting)
     
 
-
 def nominal_position(
     simulator: QuadSimulator, 
-    Kpjoin=np.diag([20,20,20]), 
-    Kdjoin=np.diag([1,1,1]), 
-    des_pos=np.array([0,0,-0.25]),
+    Kpjoin=np.diag([50,100,100]), 
+    Kdjoin=np.diag([2,2,2]), 
+    des_pos=np.array([0,0,-0.20]),
     des_vel=np.array([0,0,0])
     # OPTIONAL: add potential controller parameters here (e.g., gains)
 ) -> np.ndarray:
@@ -87,11 +86,11 @@ def nominal_position(
         foot_vel = J @ simulator.get_motor_velocities(leg_id)
 
         # hip offset
-        hip_offset = 0.25
+        hip_offset = 0.35
         if leg_id%2 == 0:  # right legs
-            des_pos[1] -= hip_offset
+            des_pos[0] -= hip_offset
         else:  # left legs
-            des_pos[1] += hip_offset
+            des_pos[0] += hip_offset
 
         tau_i = J_T @ (Kpjoin @ (des_pos - foot_pos) + Kdjoin @ (des_vel - foot_vel))
 
@@ -110,6 +109,29 @@ def virtual_model(
 
         # TODO: compute virtual model torques for leg_id
         tau_i = np.zeros(3)
+
+        first_matrix = [
+            [1, 1, -1, -1],
+            [-1, 1, -1, 1],
+            [0, 0, 0, 0]
+        ]
+        P = simulator.get_base_orientation_matrix() @ first_matrix
+        
+        # Gain que l'on peut changer 
+        K_vmc = 1
+
+        matrix_K = K_vmc*([0, 0, 1] @ P)
+        zeros_part = np.zeros((2, 4))
+        F_vmc = np.vstack([zeros_part, matrix_K])
+        #
+        #           | 0  0  0  0 |
+        # F_vmc =   | 0  0  0  0 | 
+        #           | ka kb kc kd|
+        #
+
+        # tau_i = J_i(q_i) * F_vmc_i
+        jacobian_inverse = np.linalg.inv(simulator.get_jacobian_and_position(leg_id)[0]) # Jacobienne inverse
+        tau_i = jacobian_inverse @ F_vmc[: , leg_id]
         # Store in torques array
         tau[leg_id * N_JOINTS : leg_id * N_JOINTS + N_JOINTS] = tau_i
     return tau
@@ -125,7 +147,14 @@ def gravity_compensation(
 
         # TODO: compute gravity compensation torques for leg_id
         tau_i = np.zeros(3)
+        J, foot_pos = simulator.get_jacobian_and_position(leg_id)
+        J_T = J.transpose()
 
+        g = np.zeros(3)
+        g = np.array([0,0, -9.81*simulator.get_mass()/4]) #gravity force on each leg
+
+
+        tau_i += J_T @ g
         # Store in torques array
         tau[leg_id * N_JOINTS : leg_id * N_JOINTS + N_JOINTS] = tau_i
 
